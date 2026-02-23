9bbcd2c740698cf0fa9da50c3e98273a716bc660
Viraj Vikum <virajempire2001@gmail.com>
2026-02-23 16:56:55 +0530

    updated dashboard internal count

--- app/Services/DashboardStatisticsService.php
+++ app/Services/DashboardStatisticsService.php
@@ -25,7 +25,7 @@ class DashboardStatisticsService
         $sql = "SELECT
             IFNULL(SUM(IF(a.direction = 'in' AND a.status=1, 1, 0)), 0) AS total_inbound_call_count,
             IFNULL(SUM(IF(a.direction = 'out' AND a.status=1, 1, 0)), 0) AS total_outbound_call_count,
-            IFNULL(SUM(IF(a.direction = 'ext', 1, 0)), 0) AS total_internal_call_count
+            IFNULL(SUM(IF(a.direction = 'ext'AND a.status=1, 1, 0)), 0) AS total_internal_call_count
         FROM callcount a
         WHERE a.date > CURDATE()";
 
