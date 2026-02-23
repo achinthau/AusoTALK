91198e28b2ad3b8abff69efe7901442adac549ca
Viraj Vikum <virajempire2001@gmail.com>
2026-02-23 18:41:35 +0530

    updated ongoing count api

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/call-answered', function (StoreAnsweredCall $request) {
+Route::post('/pbx-call-answered', function (StoreAnsweredCall $request) {
     Log::info($request->all());
 
     $number = $request['ani'];
@@ -16,6 +16,17 @@ Route::post('/call-answered', function (StoreAnsweredCall $request) {
     Cache::forever('agent-in-call-'.$agent->id, 1);
     Cache::forever('call-'.$request['unique_id'], $agent->id);
     Cache::add('current-call-count', 0, 99999999);
+
+    return response()->json(['status' => 'ok']);
+});
+
+Route::post('/pbx-call-disconnected', function (StoreAnsweredCall $request) {
+    Log::info($request->all());
+
+    $agent = User::where('extension', $request['agent'])->first();
+
+    Cache::forget('agent-in-call-'.$agent->id);
+    Cache::forget('call-'.$request['unique_id']);
 
     return response()->json(['status' => 'ok']);
 });
