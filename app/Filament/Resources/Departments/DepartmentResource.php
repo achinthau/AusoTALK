43b0992f7fd5f2d5f49a5e35b8c45a3c42f5798b
Viraj Vikum <virajempire2001@gmail.com>
2026-03-16 15:39:53 +0530

    Updated role base restrictions for data

--- app/Filament/Resources/Departments/DepartmentResource.php
+++ app/Filament/Resources/Departments/DepartmentResource.php
@@ -37,11 +37,10 @@ class DepartmentResource extends Resource
     protected static function scopeByCompany($query)
     {
         $user = auth()->user();
-        // If user has a company, show only departments from that company
-        if ($user && $user->company_id) {
+        // Only restrict if not super admin
+        if ($user && $user->company_id && ! $user->hasRole('super_admin')) {
             return $query->where('company_id', $user->company_id);
         }
-
         // Super admin sees all departments
         return $query;
     }
